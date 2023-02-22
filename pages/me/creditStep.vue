d97ced605abdfaf48915aed87490ec1a82402c6b
杜印 <m18612326243@163.com>
2023-02-22 11:17:27 +0800

    添加页面功能模块

--- pages/me/creditStep.vue
+++ pages/me/creditStep.vue
@@ -2,7 +2,7 @@
  * @Author: 杜印 m18612326243@163.com
  * @Date: 2022-12-24 18:52:18
  * @LastEditors: 杜印 m18612326243@163.com
- * @LastEditTime: 2023-02-21 15:32:07
+ * @LastEditTime: 2023-02-22 10:55:59
  * @FilePath: /orz-uniapp/pages/index/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -264,10 +264,13 @@
   				this.count = index
 			},
 			nextHandle(){
-				if(this.currentIndex==5){
-					this.currentIndex = 5
+				
+				
+				if(this.currentIndex==4){
+					this.currentIndex = 4
+				}else{
+					this.currentIndex = this.currentIndex+1
 				}
-				this.currentIndex = this.currentIndex+1
 			},
 			codeChange(text) {
 				this.tips = text;
